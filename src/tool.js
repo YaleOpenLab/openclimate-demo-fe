a0e3ef210c12139ec3e0a5529b9e4f6db18108a6
amandapat <pattersonamanda091@gmail.com>
2019-07-08 14:45:54 -0400

    Changed public tool

--- src/tool.js
+++ src/tool.js
@@ -5,13 +5,29 @@ import { NavLink, Switch, Route } from 'react-router-dom';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import axios from "axios";
+
+
+const API_URL = 'http://localhost:8001';
+const username = 'amanda';
+const pwhash = '9a768ace36ff3d1771d5c145a544de3d68343b2e76093cb7b2a8ea89ac7f1a20c852e6fc1d71275b43abffefac381c5b906f55c3bcff4225353d02f1d3498758';
 
 
 class Tool extends Component{	
+  componentDidMount() {
+    const url = `${API_URL}/carbon/budget?username=${username}&pwhash=${pwhash}`;
+    axios.get(url).then(response => response.data)
+    .then((data) => {
+      this.setState({fuel: data["2017"]["Fossil-Fuel-And-Industry"], landuse: data["2017"]["Land-Use-Change-Emissions"], 
+        atmos: data["2017"]["Atmospheric-Growth"], ocean: data["2017"]["Ocean-Sink"], landsink: data["2017"]["Land-Sink"], 
+        budget: data["2017"]["Budget-Imbalance"]}) 
+    })
+  }
 	constructor(props, context) {
 	  super(props, context);
-	  this.state = { visibleEarth: false };
-	  this.state = { visibleNation: false };
+	  this.state = { 
+      visibleEarth: false, visibleNation: false,
+      fuel: 0, landuse: 0, atmos: 0, ocean: 0, landsink: 0, budget: 0 };
 	  this.toggleEarth = this.toggleEarth.bind(this);
 	  this.toggleNation = this.toggleNation.bind(this);
 	}
@@ -27,8 +43,10 @@ class Tool extends Component{
 		return (
 			<div className = 'tool'>
 				<NavLink to="/actorpage#account"><button className="login">LOG IN</button></NavLink>
-				<h1>This is the PUBLIC tool component (login from here) </h1>
-				<EarthMenu toggleEarth={this.toggleEarth} earthVisibility={this.state.visibleEarth}/>
+        <h2>Fun title! wooh</h2>
+				<EarthMenu toggleEarth={this.toggleEarth} earthVisibility={this.state.visibleEarth} 
+          fuelData = {this.state.fuel} landuseData = {this.state.landuse} atmosData = {this.state.atmos} 
+          oceanData = {this.state.ocean} landsinkData = {this.state.landsink} budgetData = {this.state.budget}/>
 				<NationMenu toggleNation={this.toggleNation} natlVisibility={this.state.visibleNation}/>
 			</div>
 		)
@@ -38,7 +56,6 @@ class Tool extends Component{
 export default Tool;
 
 
-
 class EarthMenu extends Component {
   render() {
     var visibility = "hide";
@@ -46,13 +63,25 @@ class EarthMenu extends Component {
     return (
       <div id="earthMenu" className={visibility}>
       	<div className="myLabel">
-      		<button id="slideButton" onClick={this.props.toggleEarth}>EARTH</button>
+      		<button className="labelButton">EARTH</button>
       	</div>
-      	<h2><a href="#">Home</a></h2>
+        <div>
+          <button id="slideButton" onClick={this.props.toggleEarth} > Expand ></button>
+          <h5>Emissions from fossil fuel combustion and industrial processes 
+            (uncertainty of ±5% for a ± 1 sigma confidence level): {this.props.fuelData} </h5>
+          <h5>Emissions from land-use change (uncertainty of ±0.7 GtC/yr): {this.props.landuseData}</h5>
+          <h5>The atmospheric CO2 growth rate (variable uncertainty around 0.2 GtC/yr from 1980): {this.props.atmosData}</h5>
+          <h5>The ocean sink (uncertainty of ±0.5 GtC/yr): {this.props.oceanData}</h5>
+          <h5>The land sink (uncertainty of ±0.9 GtC/yr on average): {this.props.landsinkData}</h5>
+          <h5>The budget imbalance is the sum of emissions (fossil fuel and industry + land-use change) 
+          minus (atmospheric growth + ocean sink + land sink) {this.props.budgetData}</h5>
+          <h6>(data reported in 2017)</h6>
+        </div>
       </div>
     );
   }
 }
+
 
 class NationMenu extends Component {
   render() {
@@ -60,12 +89,12 @@ class NationMenu extends Component {
     if (this.props.natlVisibility ) { visibility = "show"; }
     return (
       <div id="nationMenu" className={visibility}>
-      	<div className="myLabel">
-      		<button id="slideButton" onClick={this.props.toggleNation}>NATION STATES</button>
-      	</div>
+        <div className="myLabel">
+          <button className="labelButton">NATION STATES</button>
+        </div>
       	<Accordion id="myAccordian">
 				  <Card>
-				    <Accordion.Toggle as={Card.Header} eventKey="0" className="listButton">
+				    <Accordion.Toggle as={Card.Header} eventKey="0" className="listButton" >
 				      NATION STATES
 				    </Accordion.Toggle>
 				    <Accordion.Collapse eventKey="0">
@@ -75,7 +104,7 @@ class NationMenu extends Component {
 				    </Accordion.Collapse>
 				  </Card>
 				  <Card>
-				    <Accordion.Toggle as={Card.Header} eventKey="1" className="listButton">
+				    <Accordion.Toggle as={Card.Header} eventKey="1" className="listButton" >
 				      MULTINATIONALS
 				    </Accordion.Toggle>
 				    <Accordion.Collapse eventKey="1">
@@ -83,6 +112,7 @@ class NationMenu extends Component {
 				    </Accordion.Collapse>
 				  </Card>
 				</Accordion>
+        <button id="slideButton" onClick={this.props.toggleNation} > Expand ></button>
       </div>
     );
   }
