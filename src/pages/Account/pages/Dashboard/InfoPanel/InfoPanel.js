674399d51392af222e367e622ef77ad070da8126
Tornike Mamatsashvili <morsmetus@gmail.com>
2019-09-05 19:27:24 +0400

    Add info panel component

---
+++ src/pages/Account/pages/Dashboard/InfoPanel/InfoPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./InfoPanel.scss";
+
+const InfoPanel = ({}) => {
+    return (
+        <div className='info-panel'>
+
+        </div>
+    )
+};
+
+export default InfoPanel;
