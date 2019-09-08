e03535ca1ca6a478885e6e76b6918890900be85f
Tornike Mamatsashvili <morsmetus@gmail.com>
2019-09-09 00:47:08 +0400

    Change disclaimer message

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,37 +1,29 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./Footer.scss";
 
 class Footer extends Component {
-  render() {
-    return (
-      <div className="footer">
-        <div className="footer-menu"></div>
-        <div className="footer-disclaimer">
-          <div className="disclaimer">
-            <p>
-              <b>Disclaimer:</b> Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Planet Earth’s atmosphere can hold a limited amount of
-              CO2e emissions before raising average global temperatures above a
-              safe space for humanity. This limited quantity of emissions has
-              been termed our carbon budget. The threshold level that determines
-              this budget is derived from physical properties in radiative
-              forcing (i.e. greenhouse gas effect) and its thoroughly studied
-              effects on global climate. The Paris Agreement of 2015
-              consolidated a global effort to maintain this threshold to 1.5oC
-              warming relative to pre-industrial levels. If present emission
-              pathways are left unchecked, the budget could be consumed in as
-              little as 15 years. To achieve the ambitions set in Paris, CO2
-              emission must peak by 2020 and then decline radically, arriving to
-              net-zero by 2050 and negative values by the end of the century.
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="footer">
+                <div className="footer-menu"></div>
+                <div className="footer-disclaimer">
+                    <div className="disclaimer">
+                        <p>
+                            Disclaimer: This Open Climate portal is an alpha prototype being developed as part of a
+                            broader open source project, which explore the use of distributed ledger technologies (DLT)
+                            and other emerging technologies to enable a transparent global climate accounting system.
+                            This version is undergoing constant updates and may not reflect the full state of the
+                            project. More established versions might be hosted in an internal url for staging and
+                            sandboxing purpose. Please <a className='oc-link' href="mailto:martin.wainstein@yale.edu">contact us</a> for any question related
+                            to the current state of the project and platform. Whilst the platform is currently incubated
+                            at the Yale Open Innovation Lab, it combines other platforms and represents a decentralized
+                            open source initiative. <a className='oc-link' href="http://www.openlab.yale.edu/open-climate">Learn more ></a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Footer;
